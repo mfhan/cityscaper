adedd86bec1e2137f6d3ac0403f7a3bca156c104
Andre Williams <mMmCheesecake@Andres-MacBook-Pro.local>
2019-09-16 16:20:39 -0400

    add usercontroller code to server

--- server.js
+++ server.js
@@ -4,31 +4,31 @@ const cors = require('cors');
 const logger = require('morgan');
 const tourController = require('./controllers/tourController');
 const locationController = require('./controllers/locationController');
-//const userController = require('./controllers/userController');
+const userController = require('./controllers/userController');
 
 const PORT = process.env.PORT || 3001;
-//define the initial variable
+// define the initial variable
 const app = express();
 
-//set up middleware
+// set up middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use('/tours', tourController);
 app.use('/locations', locationController);
-//app.use('/auth', userController);
-//catch-all:
-app.use((e, req, res, next) =>{
+app.use('/auth', userController);
+// catch-all:
+app.use((e, req, res, next) => {
   if (e) {
     console.log(e.message);
     next(e);
   }
-})
+});
 
-//necessary routes
+// necessary routes
 app.get('/', (req, res) => {
   res.send('Welcome to CityScaper!');
 });
 
-//our listen
+// our listen
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
