7299220c9377a1666d470d93090b36aa042b7ffc
Andrew Hollingworth <andrewhollingworth@Andrews-MacBook-Air.local>
2019-09-16 17:04:00 -0400

    server.js updates

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const cors = require('cors');
 const logger = require('morgan');
 const tourController = require('./controllers/tourController');
 const locationController = require('./controllers/locationController');
-//const userController = require('./controllers/userController');
+const userController = require('./controllers/userController');
 
 const PORT = process.env.PORT || 3001;
 //define the initial variable
 const app = express();
 
 //set up middleware
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(logger('dev'));
-// app.use('/tours', tourController);
-// app.use('/locations', locationController);
-//app.use('/auth', userController);
+app.use('/tours', tourController);
+app.use('/locations', locationController);
+app.use('/auth', userController);
 //catch-all:
 app.use((e, req, res, next) =>{
   if (e) {
