a761915ca1a280497ae453357044d138ad8224de
Andrew Hollingworth <andrewhollingworth@Andrews-MacBook-Air.local>
2019-09-16 15:07:56 -0400

    Add user data and complete models.

--- data.js
+++ data.js
@@ -18,36 +18,42 @@ const locationdata = [
     name: "The Flatiron Building",
     description: "The not-quite-skinniest building in NYC.",
     location: "LOCATION DATA",
-    image: "https://s3-media2.fl.yelpcdn.com/bphoto/FdXvDh2F1Yn8ObZ6amItPQ/l.jpg",
+    image: "https://s3-media2.fl.yelpcdn.com/bphoto/FdXvDh2F1Yn8ObZ6amItPQ/l.jpg"
   },
   {
-    name:"namek",
-    color:"blue",
-    num_moons: 9001
+    name: "One World Trade",
+    description: "The new world trade center building.",
+    location: "LOCATION DATA",
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/a1/One_World_Trade_Center_May_2015.jpg"
   },
   {
-    name:"pluto",
-    color:"blue",
-    num_moons: 0
+    name: "Russ and Daughters Cafe",
+    description: "Delicious eats, retro vibe, and a classic New York experience.",
+    location: "LOCATION DATA",
+    image: "https://images.squarespace-cdn.com/content/v1/57be0600e3df288b8ce9492a/1553032863153-7I666EE0UMUGV6AA9S0Y/ke17ZwdGBToddI8pDm48kFTEgwhRQcX9r3XtU0e50sUUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcW7uEhC96WQdj-SwE5EpM0lAopPba9ZX3O0oeNTVSRxdHAmtcci_6bmVLoSDQq_pb/static.jpg?format=2500w"
   }
 ];
 
 const userdata = [
   {
-    name:"endor",
-    color:"green",
-    num_moons: 5
+    username:"ahollingworth",
+    email:"andrew.hollingworth@gmail.com",
+    password_digest: "1234"
   },
   {
-    name:"namek",
-    color:"blue",
-    num_moons: 9001
+    username:"mfhan",
+    email:"mfhan@hotmail.com",
+    password_digest: "qwerty"
   },
   {
-    name:"pluto",
-    color:"blue",
-    num_moons: 0
+    username:"drecheesecake",
+    email:"drelovescheesecake@aol.com",
+    password_digest: "asdf"
   }
 ];
 
-module.exports = data;
+module.exports = {
+  userdata,
+  locationdata,
+  tourdata,
+};
