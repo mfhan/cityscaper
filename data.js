4f0c3f388ebec63b00b262b5f7eb204869b83656
Andrew Hollingworth <andrewhollingworth@Andrews-MacBook-Air.local>
2019-09-16 14:55:59 -0400

    Create sample tour and location data.

---
+++ data.js
@@ -0,0 +1,53 @@
+const tourdata = [
+  {
+    name:"LES Foodies",
+    description:"A tour of the best eats in the Lower East Side. Locals only!"
+  },
+  {
+    name:"Manhatten Henge",
+    description:"A list of historical locations in Manhattan that provide historical context for the greatest city in the world."
+  },
+  {
+    name:"Scav-NYC",
+    description:"New York's most famous scavenger hunt."
+  },
+];
+
+const locationdata = [
+  {
+    name: "The Flatiron Building",
+    description: "The not-quite-skinniest building in NYC.",
+    location: "LOCATION DATA",
+    image: "https://s3-media2.fl.yelpcdn.com/bphoto/FdXvDh2F1Yn8ObZ6amItPQ/l.jpg",
+  },
+  {
+    name:"namek",
+    color:"blue",
+    num_moons: 9001
+  },
+  {
+    name:"pluto",
+    color:"blue",
+    num_moons: 0
+  }
+];
+
+const userdata = [
+  {
+    name:"endor",
+    color:"green",
+    num_moons: 5
+  },
+  {
+    name:"namek",
+    color:"blue",
+    num_moons: 9001
+  },
+  {
+    name:"pluto",
+    color:"blue",
+    num_moons: 0
+  }
+];
+
+module.exports = data;
